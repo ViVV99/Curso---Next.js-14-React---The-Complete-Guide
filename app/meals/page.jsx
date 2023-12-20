00b1fe7b122f6147eb96a604d1d7905cff75c0b4
Vitor <vitorvv99@gmail.com>
2023-12-19 22:45:00 -0300

    add: meal details page

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
 
 export default function MealsPage() {
   return (
@@ -9,14 +10,16 @@ export default function MealsPage() {
           Delicious meals, created
           <span className={classes.highlight}>by you</span>
         </h1>
-        <p>Choose your favorite recipe and cook it yourself. It is easy and fun</p>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
         <p className={classes.cta}>
-            <Link href="/meals/share">
-                Share Your Favorite Recipe
-            </Link>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
