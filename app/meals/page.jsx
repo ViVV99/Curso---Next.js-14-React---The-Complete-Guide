6a4ee8a278990a6a9279ea50467f86639b266e4c
Vitor <vitorvv99@gmail.com>
2023-12-19 22:39:03 -0300

    add: slideshow and dynamic navbar links

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,12 +1,22 @@
-import Link from "next/link";
+import Link from 'next/link';
+import classes from './page.module.css';
 
 export default function MealsPage() {
-    return (
-        <main>
-            <h1>Welcome to the meals page!</h1>
-            <Link href="meals/share">Share meals</Link>
-            <Link href="meals/community">Talk about meals</Link>
-            <Link href="meals/secret-route">Its secret!</Link>
-        </main>
-    )
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun</p>
+        <p className={classes.cta}>
+            <Link href="/meals/share">
+                Share Your Favorite Recipe
+            </Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 }
